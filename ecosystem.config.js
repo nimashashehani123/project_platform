6682e34dd89a34d0bb1f455064a594ed33e03c47
T.L. Nimasha Shehani <156079694+nimashashehani123@users.noreply.github.com>
2026-03-31 05:48:44 +0530

    git commit -m "update ecosystem.config.js"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,29 @@
+const path = require("path");
+
+const isWin = process.platform === "win32";
+
 module.exports = {
     apps: [
         {
             name: "config-server",
-            script: "C:\\Users\\Nimasha Shehani\\.jdks\\ms-25.0.2\\bin\\java.exe",
-            args: "-jar ./config-server/target/Config-Server-1.0.0.jar"
+            script: "java",
+            args: `-jar ${isWin ? ".\\config-server\\target\\Config-Server-1.0.0.jar" : "./config-server/target/Config-Server-1.0.0.jar"}`,
+            cwd: __dirname,
+            watch: false
         },
         {
             name: "service-registry",
-            script: "C:\\Users\\Nimasha Shehani\\.jdks\\ms-25.0.2\\bin\\java.exe",
-            args: "-jar ./service-registry/target/Service-Registry-1.0.0.jar"
+            script: "java",
+            args: `-jar ${isWin ? ".\\service-registry\\target\\Service-Registry-1.0.0.jar" : "./service-registry/target/Service-Registry-1.0.0.jar"}`,
+            cwd: __dirname,
+            watch: false
         },
         {
             name: "api-gateway",
-            script: "C:\\Users\\Nimasha Shehani\\.jdks\\ms-25.0.2\\bin\\java.exe",
-            args: "-jar ./api-gateway/target/Api-Gateway-1.0.0.jar"
+            script: "java",
+            args: `-jar ${isWin ? ".\\api-gateway\\target\\Api-Gateway-1.0.0.jar" : "./api-gateway/target/Api-Gateway-1.0.0.jar"}`,
+            cwd: __dirname,
+            watch: false
         }
     ]
 };
